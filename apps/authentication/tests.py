84cf59ed9834849e1c20a55cc4e2ed0c86ddbebe
delvin07 <delpinparel@gmail.com>
2026-08-06 07:06:17 +0700

    feat: admin PenanggungJawab dengan form create user

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -24,3 +24,25 @@ class PenanggungJawabModelTest(TestCase):
         u2 = User.objects.create_user('pj2', password='x')
         pj = PenanggungJawab.objects.create(user=u2, nama_lengkap='C')
         self.assertTrue(pj.aktif)
+
+
+class PenanggungJawabAdminCreateTest(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser('root', 'r@x.com', 'pw')
+        self.client.force_login(self.admin)
+
+    def test_tambah_membuat_user_dan_password(self):
+        response = self.client.post(
+            '/admin/authentication/penanggungjawab/add/',
+            {
+                'username': 'pj_baru',
+                'password': 'pass1234',
+                'nama_lengkap': 'Sari',
+                'telepon': '0812',
+                'aktif': 'on',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        user = User.objects.get(username='pj_baru')
+        self.assertTrue(user.check_password('pass1234'))
+        self.assertTrue(PenanggungJawab.objects.filter(user=user).exists())
